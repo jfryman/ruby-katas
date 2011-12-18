de961ce1739536e407fd1c721de628beaad5c538
James Fryman <james@frymanet.com>
2011-12-18 17:28:58 -0600

    working karate chop

--- codekata/karate_chop/pass_1/spec/game_spec.rb
+++ codekata/karate_chop/pass_1/spec/game_spec.rb
@@ -14,28 +14,68 @@ describe "#chop" do
   it "returns first element when a match is in the first element" do
     game.chop(1, [1]).should == 0
   end
-end
 
-def test_chop
-    assert_equal(-1, chop(3, []))
-    assert_equal(-1, chop(3, [1]))
-    assert_equal(0,  chop(1, [1]))
-    #
-    assert_equal(0,  chop(1, [1, 3, 5]))
-    assert_equal(1,  chop(3, [1, 3, 5]))
-    assert_equal(2,  chop(5, [1, 3, 5]))
-    assert_equal(-1, chop(0, [1, 3, 5]))
-    assert_equal(-1, chop(2, [1, 3, 5]))
-    assert_equal(-1, chop(4, [1, 3, 5]))
-    assert_equal(-1, chop(6, [1, 3, 5]))
-    #
-    assert_equal(0,  chop(1, [1, 3, 5, 7]))
-    assert_equal(1,  chop(3, [1, 3, 5, 7]))
-    assert_equal(2,  chop(5, [1, 3, 5, 7]))
-    assert_equal(3,  chop(7, [1, 3, 5, 7]))
-    assert_equal(-1, chop(0, [1, 3, 5, 7]))
-    assert_equal(-1, chop(2, [1, 3, 5, 7]))
-    assert_equal(-1, chop(4, [1, 3, 5, 7]))
-    assert_equal(-1, chop(6, [1, 3, 5, 7]))
-    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  it "returns first element when a match is in the first element of a 3 value array" do
+    game.chop(1, [1, 3, 5]).should == 0
   end
+
+  it "returns the second element when a match is in the first element of a 3 valle array" do
+    game.chop(3, [1, 3, 5]).should == 1
+  end
+
+  it "returns the third element when a match is in the third element of a 3 value array" do
+    game.chop(5, [1, 3, 5]).should == 2
+  end
+
+  it "returns -1 when searching for the number 0 in an odd numbered 3 element array" do
+    game.chop(0, [1, 3, 5]).should == -1
+  end
+
+  it "returns -1 when searching for the number 2 in an odd numbered 3 element array" do
+    game.chop(2, [1, 3, 5]).should == -1
+  end
+
+  it "returns -1 when searching for the number 4 in an odd numbered 3 element array" do
+    game.chop(4, [1, 3, 5]).should == -1
+  end
+
+  it "returns -1 when searching for the number 6 in an odd numbered 3 element array" do
+    game.chop(8, [1, 3, 5]).should == -1
+  end
+
+  it "returns first element when a match is in the first element of a 4 value array" do
+    game.chop(1, [1, 3, 5, 7]).should == 0
+  end
+
+  it "returns the second element when a match is in the first element of a 4 valle array" do
+    game.chop(3, [1, 3, 5, 7]).should == 1
+  end
+
+  it "returns the third element when a match is in the third element of a 4 value array" do
+    game.chop(5, [1, 3, 5, 7]).should == 2
+  end
+
+  it "returns the fourth element when a match is in the third element of a 4 value array" do
+    game.chop(7, [1, 3, 5, 7]).should == 3
+  end
+
+  it "returns -1 when searching for the number 0 in an odd numbered 4 element array" do
+    game.chop(0, [1, 3, 5, 7]).should == -1
+  end
+
+  it "returns -1 when searching for the number 2 in an odd numbered 4 element array" do
+    game.chop(2, [1, 3, 5, 7]).should == -1
+  end
+
+  it "returns -1 when searching for the number 4 in an odd numbered 4 element array" do
+    game.chop(4, [1, 3, 5, 7]).should == -1
+  end
+
+  it "returns -1 when searching for the number 6 in an odd numbered 4 element array" do
+    game.chop(6, [1, 3, 5, 7]).should == -1
+  end
+
+  it "returns -1 when searching for the number 8 in an odd numbered 4 element array" do
+    game.chop(8, [1, 3, 5, 7]).should == -1
+  end
+end
